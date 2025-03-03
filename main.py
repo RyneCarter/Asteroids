a96ce7ea533671fd15137b3cc2c60aee6b7e360a
RyneCarter <carterryne@gmail.com>
2025-03-03 16:48:44 -0600

    More updates to the files

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
   pygame.init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0.0
+
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
   running = True
   while running:
-    screen.fill((0, 0, 0))
-
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         running = False
 
+    screen.fill("black")
+    player.draw(screen)
     pygame.display.flip()
+
+    dt = clock.tick(60) / 1000
 
   pygame.quit()
 
